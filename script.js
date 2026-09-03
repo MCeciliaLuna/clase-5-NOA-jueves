a54c54435a4e7d875085f8e7d27814fe0762b67b
María Cecilia <lunama.cecilia@live.com>
2026-09-03 18:55:44 -0300

    local y session storage

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const funcionDePrueba = () => {
 //ONCHANGE EN HTML: Ejecuta la función automáticamente que nos vamos de foco del elemento, por ejemplo, um input
 //ONINPUT: captura cada carácter ingresado
 
-const email = document.getElementById("email");
-// const pass = document.getElementById("password");
-
 const capturarDato = () => {
     const emailData = email.value;
     // const passData = pass.value;
@@ -23,5 +20,44 @@ const enviarDatos = (event) => {
     event.preventDefault()
 
     const emailData = email.value;
-    console.log(emailData) 
+    console.log(emailData)
+}
+
+
+//ALMACENAMIENTO EN NAVEGADOR
+
+//LOCAL STORAGE
+//guardamos datos en la memoria LOCAL del navegador; persisten aun con el navegador cerrado en la misma URL
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+
+const guardarDatosLocal = () => {
+    const emailData = email.value;
+    const passData = pass.value;
+
+    //crea el dato en el almacenamiento local del navegador:
+    sessionStorage.setItem("emailUsuario", emailData);
+    sessionStorage.setItem("passwordUsuario", passData);
+    window.location.href = "bienvenida.html"
+}
+
+const corroborarLogin = () => {
+    //TRAER EL ITEM DESDE LOCALSTORAGE
+    const emailLocalStorage = sessionStorage.getItem("emailUsuario");
+
+    if (emailLocalStorage) {
+        window.location.href = "bienvenida.html"
+    }
+}
+
+if (window.location.pathname !== "/bienvenida.html") {
+    corroborarLogin()
+}
+
+const logout =()=>{
+    //CLEAR limpia el localstorage - borra todos los datos de esa url
+    sessionStorage.clear();
+
+    //redireccionamos a index.html
+    window.location.href = "index.html";
 }
